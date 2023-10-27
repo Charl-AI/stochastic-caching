6a008896eb57f3b0e4ef922fbddb20a320e1f644
C Jones <cj1917@ic.ac.uk>
2023-10-27 10:29:24 +0100

    allow parameters to be passed to run_sweep.sh

--- run_sweep.sh
+++ run_sweep.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# first argument is data dir
+data_dir=$1
+
+# second argument is output dir
+output_dir=$2
+
 seeds=( 1 2 3 )
 cache_sizes=( 0 1 2 3 4 5 6 7 8 9 )
 
@@ -7,7 +13,7 @@ for seed in "${seeds[@]}"
 do
     for cache_size in "${cache_sizes[@]}"
     do
-        python3 run_benchmark.py --seed "$seed" --cache-limit-gib "$cache_size"
+        python3 run_benchmark.py --seed "$seed" --cache-limit-gib "$cache_size" --data-dir "$data_dir" --output-dir "$output_dir"
     done
 done
 
